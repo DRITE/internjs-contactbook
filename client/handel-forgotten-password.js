537c23da81ea7164072e5e2516d87ea239c87187
DRITE <gubarevfyodor@gmail.com>
2017-10-28 23:57:08 +0300

    First commit.

---
+++ client/handel-forgotten-password.js
@@ -0,0 +1,6 @@
+function handleForgottenPass(){
+    alert('Ну ты лох...');
+}
+
+let forgottenPassButton = document.getElementById('quick_forgot_password');
+forgottenPassButton.addEventListener('click', handleForgottenPass);
